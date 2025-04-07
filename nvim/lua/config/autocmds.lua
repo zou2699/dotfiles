c16a6d0a07f2201953e087a88bfc2dba0e93918a
tux <zou2699@gmail.com>
2025-04-07 10:55:15 +0800

    fix markdown lint

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,11 +7,15 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+local function augroup(name)
+   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+local numbertoggle = vim.api.nvim_create_augroup("numbertoggle", {}) 
+ 
 
 vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
    pattern = "*",
-   group = augroup,
+   group = numbertoggle,
    callback = function()
       if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
          vim.opt.relativenumber = true
@@ -21,7 +25,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "Cmdline
 
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
    pattern = "*",
-   group = augroup,
+   group = numbertoggle,
    callback = function()
       if vim.o.nu then
          vim.opt.relativenumber = false
@@ -29,3 +33,13 @@ vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEn
       end
    end,
 })
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+   group = augroup("wrap_spell"),
+   pattern = { "gitcommit", "markdown" },
+   callback = function()
+     vim.opt_local.wrap = true
+     vim.opt_local.spell = false -- 覆盖默认的 true
+   end,
+ })
